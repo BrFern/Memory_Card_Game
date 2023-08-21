5a60af63fa7f1e8a5e09ccb9b17506ec1b78e063
BrFern <102929013+BrFern@users.noreply.github.com>
2023-08-21 11:35:04 -0600

    got the flip piece

--- scripts.js
+++ scripts.js
@@ -16,8 +16,19 @@ function flipCard() {
     secondCard = this;
 
    //do cards match?
-   console.log(firstCard.dataset.framework);
-   console.log(secondCard.dataset.framework);
+   if (firstCard.dataset.framework === secondCard.dataset.framework){
+    //it's a match
+     firstCard.removeEventListener('click', flipCard)
+     secondCard.removeEventListener('click', flipCard)
+   } else {
+    //not a match
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+    }, 1500)
+    
+   }
+   
   }
 }
 
